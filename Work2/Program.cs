400d278eadf44b2de8289bead9fd3e612cfce1f8
NatalyaAkhmetova <kronlogist@gmail.com>
2022-06-24 19:30:03 +0300

    add task2

---
+++ Work2/Program.cs
@@ -0,0 +1,7 @@
+﻿//Определение большего из двух чисел
+Console.Write("Введите первое число_");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число_");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB) Console.WriteLine(numberA);
+else Console.WriteLine(numberB);
