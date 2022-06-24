1dc0ca4643ad29135290748dbbfbd4b7c58bc8e8
NatalyaAkhmetova <kronlogist@gmail.com>
2022-06-24 20:06:18 +0300

    add task6

---
+++ Work6/Program.cs
@@ -0,0 +1,5 @@
+﻿// Определение: является ли число четным
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n%2 == 0) Console.WriteLine("Число является четным.");
+else Console.WriteLine("Число является нечетным.");
