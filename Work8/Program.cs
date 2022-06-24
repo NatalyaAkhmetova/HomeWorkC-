7bf35a5a9f9342b8d86bd845b14da16d215acaca
NatalyaAkhmetova <kronlogist@gmail.com>
2022-06-24 20:30:25 +0300

    add task8

---
+++ Work8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывод четных чисел от 1 до N
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while(i <= number)
+{
+   if (i%2 == 0) Console.WriteLine(i);
+   i = i + 1;   
+}
