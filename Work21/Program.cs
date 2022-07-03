c20a968050a0fa728a2d75e9bc0d27f35f5b6218
NatalyaAkhmetova <kronlogist@gmail.com>
2022-07-03 16:10:33 +0300

    add Work 21

---
+++ Work21/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+double xa, ya, xb, yb, za, zb, len;
+xa = Convert.ToDouble(Console.ReadLine());
+ya = Convert.ToDouble(Console.ReadLine());
+za = Convert.ToDouble(Console.ReadLine());
+xb = Convert.ToDouble(Console.ReadLine());
+yb = Convert.ToDouble(Console.ReadLine());
+zb = Convert.ToDouble(Console.ReadLine());
+len = Math.Sqrt((xa - xb)*(xa - xb) + (ya - yb)*(ya - yb) + (za - zb)*(za - zb));
+Console.WriteLine(len);
+
