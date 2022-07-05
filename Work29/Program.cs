1f9fd65e3b5cabda7142baecdaec213e29fb737a
NatalyaAkhmetova <kronlogist@gmail.com>
2022-07-05 22:48:22 +0300

    add Work 29

---
+++ Work29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите элемент №" + (i + 1));
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    
+}
+Console.WriteLine();
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+PrintArray(array);
+//Console.WriteLine(array[i]);
