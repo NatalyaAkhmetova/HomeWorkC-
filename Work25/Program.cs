5305079a8c62c2d854a61678c4f14cdaf51218fb
NatalyaAkhmetova <kronlogist@gmail.com>
2022-07-05 20:21:21 +0300

    add Work 25

---
+++ Work25/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+/*Console.WriteLine("Введите число A:_");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:_");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("A^B = " + Math.Pow(A, B));*/
+
+
+/*Console.WriteLine("Введите число A:_");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:_");
+int B = Convert.ToInt32(Console.ReadLine());
+int dgrA = A, i = 1;
+while (i < B)
+{
+    dgrA = dgrA * A;
+    i++;
+}
+Console.WriteLine(dgrA);*/
+
+
+Console.WriteLine("Введите число A:_");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:_");
+int B = Convert.ToInt32(Console.ReadLine());
+int dgrA = A;
+for (int i = 1; i < B; i++)
+{
+    dgrA = dgrA * A;
+}
+Console.WriteLine(dgrA);
