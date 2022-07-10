9a1b84e8599d51c14cd748a99106237a0d4d326d
NatalyaAkhmetova <kronlogist@gmail.com>
2022-07-10 17:36:35 +0300

    add Work 34

---
+++ Work34/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] array = new int[10];
+for (int i = 0; i < 10; i++)
+{
+  array[i] = new Random().Next(100, 1000);
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+int n = 0;
+for (int i = 0; i < 10; i++)
+{
+  if (array[i]% 2 ==0) n = n + 1;
+}
+Console.WriteLine(n);
