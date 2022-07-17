835e38ab31e92010479787ab8a6f8631ddc683cd
NatalyaAkhmetova <kronlogist@gmail.com>
2022-07-17 19:27:05 +0300

    add Work 52

---
+++ Work52/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 52. 
+//Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+int[,] GreatMatr(int n, int m)
+{
+    int[,] matrix = new int[n, m];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatr(int [, ] matrix)
+{
+    int[] sumCol = new int[3];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+       for (int j = 0; j < matrix.GetLength(1); j++)
+       {
+           Console.Write(matrix[i, j] + " \t");
+           sumCol[i] += matrix[j, i];
+       }
+       Console.WriteLine(); 
+    }
+    Console.WriteLine();
+    foreach (double elem in sumCol)
+    {
+        Console.WriteLine("Среднее арифметическое столбца равно:" + elem/3);
+    }
+}
+PrintMatr(GreatMatr(3, 3));
+
