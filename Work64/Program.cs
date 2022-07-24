73692d4b26f419249ceaf2e5d5366803f60d98e8
NatalyaAkhmetova <kronlogist@gmail.com>
2022-07-24 17:22:46 +0300

    add Work 64

---
+++ Work64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+void f(int n, int m)
+{   
+    if (n != (m - 1))
+    { 
+       f(n - 1, m);
+       Console.Write(n + " ");    
+    }
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+f(n, m);
